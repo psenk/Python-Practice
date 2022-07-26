c4cbe94ed31a8d62f1d25e07da1846be0355d619
psenk <96640001+psenk@users.noreply.github.com>
2022-07-26 18:57:07 -0500

    Create junk_code.py

---
+++ Discord Bot/junk_code.py
@@ -0,0 +1,13 @@
+"""
+# posting to id channel specifically
+channel = client.get_channel(986537383250001940)
+await channel.send("xxx")
+"""
+
+"""
+# reacting to user with emoji
+if message.author.id == 545728431917236226:
+    react_emoji = "👍"
+    # react_emoji = "<":custom_emoji:idnumber">
+    await message.add_reaction(react_emoji)
+"""
